996401d843bfb6193e01a5712bbdece208dc9cbb
dingrui <dingrui198424@126.com>
2015-01-16 15:36:08 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURRENT_TIME=`date '+%Y-%m-%d-%H-%M-%S'`
+# backup files: .vimrc && .vim
+if [ -e ~/.vimrc ]; then
+    echo "backup your .vimrc"
+    echo "this is backup by dingrui on $CURRENT_TIME">>~/.vimrc
+    mv ~/.vimrc{,.$CURRENT_TIME}
+fi
+if [ -d ~/.vim ]; then
+    echo "backup your .vim"
+    echo "this is backup by dingrui on $CURRENT_TIME">>~/.vim/README
+    mv ~/.vim{,.$CURRENT_TIME}
+fi
+
+# git clone vundle
+command -v git >/dev/null 2>&1 || { echo >&2 "Required git but it's not installed.  Aborting."; exit 1; }  
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git@github.com:lmule/vim.git ~/.vim/vimrc
+# establing soft link of .vimrc
+ln -s ~/.vim/vimrc/.vimrc ~/.vimrc
+# installing vim plugins
+vim +BundleInstall +qa
