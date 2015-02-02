03a549392306feff8dac6ff73a2db1ec5583d6bb
lmule <dingrui198424@126.com>
2015-02-02 19:43:11 +0800

    update vim.git protocal

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ fi
 command -v git >/dev/null 2>&1 || { echo >&2 "Required git but it's not installed.  Aborting."; exit 1; }  
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone git@github.com:lmule/vim.git ~/.vim/vimrc
+git clone https://github.com/lmule/vim.git ~/.vim/vimrc
 
 # establing soft link of .vimrc
 ln -s ~/.vim/vimrc/.vimrc ~/.vimrc
